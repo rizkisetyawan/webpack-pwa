6c1d05416686dc2ca3434f8ef89730b4b950b670
rizki <rizki.delameta19@gmail.com>
2021-11-05 16:40:43 +0700

    dynamic retaurant

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,5 +1,6 @@
 import '../styles/main.css';
 import '../styles/responsive.css';
+import data from '../DATA.json';
 
 const menu = document.querySelector('#menu');
 const hero = document.querySelector('.hero');
@@ -18,4 +19,23 @@ hero.addEventListener('click', function () {
 main.addEventListener('click', function () {
     drawer.classList.remove('open');
 });
+
+let restaurantElement = '';
+data.restaurants.forEach(resto => {
+    restaurantElement += `
+    <article class="post-item">
+        <p class="post-item__city">${resto.city}</p>
+        <img class="post-item__thumbnail"
+            src="${resto.pictureId}"
+            alt=${resto.name}>
+        <div class="post-item__content">
+            <p class="post-item__rate">Rate | <span class="post-item__rate_star">&#9733;</span> ${resto.rating}</p>
+            <h3 class="post-item__title">${resto.name}</h3>
+            <p class="post-item__description">${resto.description.substr(0, 150)} ...</p>
+        </div>
+    </article>
+    `
+});
+
+document.querySelector(".posts").innerHTML = restaurantElement;
 
